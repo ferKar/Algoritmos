85d008eb3be4ffe1755943532bee0fdb91ea349f
ferKar <fer.kar@ciencias.unam.mx>
2018-02-04 17:38:41 -0600

    Borrar incesasarios

--- Tarea_Creo/Problema_UNO.java
+++ Tarea_Creo/Problema_UNO.java
@@ -22,7 +22,7 @@ public class Problema_UNO{
 	}
 
     
-	public static LinkedList<Integer> suma_hojas2(int meta){
+	public static LinkedList<Integer> suma_hojas(int meta){
 	    LinkedList<Integer> paginas_arrancadas = new LinkedList<>();
 
 		for(int i = 400; i > 0; i = i - 2){
@@ -37,44 +37,14 @@ public class Problema_UNO{
 		return paginas_arrancadas;
 	}
 
-	public static LinkedList<Integer> llena_paginas(){
-		LinkedList<Integer> paginas = new LinkedList<>();
-		for(int i = 1; i <= 400; i++)
-			paginas.add(i);
-		return paginas;		
-	}
-
-	private static LinkedList<Integer> paginas = llena_paginas();
-	private static int i = 0;
-
-
-	public static LinkedList<Integer> arranca_paginas(LinkedList<Integer> paginas_arrancadas){
-		int sumaHoja = paginas.size() + (paginas.size() -1);
-
-		if(i + sumaHoja <= 2018){
-			i += sumaHoja;
-			paginas_arrancadas.add(paginas.removeLast());
-			paginas_arrancadas.add(paginas.removeLast());
-			return arranca_paginas(paginas_arrancadas);
-		}
-		else if (paginas.size() > 2){
-			paginas.removeLast();
-			paginas.removeLast();
-			return arranca_paginas(paginas_arrancadas);			
-		}else{
-			return paginas_arrancadas;
-		}
-	}
-
 	public static void main(String args[]){
-		LinkedList<Integer> paginas_arrancadas = new LinkedList<>();
 		int meta = Integer.parseInt(args[0]);
 		int regreso = 0;
 		int i = 1;
-		 /*arranca_paginas(paginas_arrancadas*/ 
-		for (Integer pag :suma_hojas2(meta)){
+
+		for (Integer pag :suma_hojas(meta)){
 			regreso += pag;
-			System.out.println("(" + (i++)  + ") :"+pag);
+			System.out.println("(" + (i++)  + ") :" + pag);
 		}
 
 		System.out.printf("La suma da: %d\n",regreso);
